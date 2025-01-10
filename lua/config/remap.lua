de7ab4608ddaca1ff307081f17114b2dd03f1e12
LDprg <lukas_4dr@gmx.at>
2025-01-10 22:41:09 +0100

    Add move lin up down keys

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "q", "<nop>")
 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+
 vim.keymap.set("n", "<leader>ß", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "Lsp toggle inly hint" })
@@ -8,4 +11,3 @@ end, { desc = "Lsp toggle inly hint" })
 vim.keymap.set("n", "<leader>d", vim.lsp.buf.code_action, { desc = "Lsp code action" })
 vim.keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", { desc = "Lsp format" })
 vim.keymap.set("n", "<leader>r", "<cmd>LspRestart<cr>", { desc = "Lsp restart" })
-

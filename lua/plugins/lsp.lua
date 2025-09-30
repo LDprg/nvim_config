72ec5683acf749160e0ce8fa402ad419cf0c0d03
LDprg <lukas_4dr@gmx.at>
2025-09-30 20:33:54 +0200

    Add back rusteacanvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,11 +5,11 @@ return {
         "mason-org/mason.nvim",
         "mason-org/mason-lspconfig.nvim",
         "j-hui/fidget.nvim",
-        -- {
-        --     'mrcjkb/rustaceanvim',
-        --     version = '^6', -- Recommended
-        --     lazy = false,   -- This plugin is already lazy
-        -- },
+        {
+            'mrcjkb/rustaceanvim',
+            version = '^6', -- Recommended
+            lazy = false,   -- This plugin is already lazy
+        },
         'saghen/blink.cmp',
     },
     opts = {
@@ -40,62 +40,125 @@ return {
         require("fidget").setup({})
         require("mason").setup({})
         require("mason-lspconfig").setup({
-            automatic_enable = true,
+            automatic_enable = {
+                exclude = {
+                    "rust_analyzer",
+                }
+            },
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer@nightly",
                 "gopls",
             },
-            handlers = {
-                ["zls"] = function()
-                    local lspconfig = require("lspconfig")
-                    vim.lsp.config("zls", {
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
+        })
+
+        local lspconfig = require("lspconfig")
+        vim.lsp.config("zls", {
+            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+            settings = {
+                zls = {
+                    enable_inlay_hints = true,
+                    enable_snippets = true,
+                    warn_style = true,
+                },
+            },
+        })
+        vim.g.zig_fmt_parse_errors = 0
+        vim.g.zig_fmt_autosave = 0
+
+        vim.lsp.config("clangd", {
+            root_dir = lspconfig.util.root_pattern(".git", ".clangd"),
+            filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+            cmd = {
+                "clangd",
+                "--all-scopes-completion",
+                "--background-index",
+                "--clang-tidy",
+                "--completion-parse=always",
+                "--completion-style=detailed",
+                "--cross-file-rename",
+                "--enable-config",
+                "--fallback-style=llvm",
+                "--function-arg-placeholders",
+                "--pch-storage=memory",
+                "-j=8",
+                "--log=verbose",
+            },
+        })
+
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        global = { "bit", "vim", "it", "describe", "before_each", "after_each", "Snacks" },
+                    }
+                }
+            }
+        })
+
+        vim.g.rustaceanvim = {
+            server = {
+                default_settings = {
+                    ["rust-analyzer"] = {
+                        procMacro = {
+                            ignored = {
+                                leptos_macro = {
+                                    -- optional: --
+                                    -- "component",
+                                    "server",
+                                },
                             },
                         },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                ["lua_ls"] = function()
-                    vim.lsp.config("lua_ls", {
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    global = { "bit", "vim", "it", "describe", "before_each", "after_each", "Snacks" },
+                        cargo = {
+                            features = "all",
+                        },
+                        workspace = {
+                            symbol = {
+                                search = {
+                                    kind = "all_symbols",
                                 }
                             }
-                        }
-                    })
-                end,
-                ["clangd"] = function()
-                    local lspconfig = require("lspconfig")
-                    vim.lsp.config("clangd", {
-                        root_dir = lspconfig.util.root_pattern(".git", ".clangd"),
-                        filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-                        cmd = {
-                            "clangd",
-                            "--all-scopes-completion",
-                            "--background-index",
-                            "--clang-tidy",
-                            "--completion-parse=always",
-                            "--completion-style=detailed",
-                            "--cross-file-rename",
-                            "--enable-config",
-                            "--fallback-style=llvm",
-                            "--function-arg-placeholders",
-                            "--pch-storage=memory",
-                            "-j=8",
-                            "--log=verbose",
                         },
-                    })
+                        checkOnSave = {
+                            enable = vim.g.use_bacon_lsp == false,
+                        },
+                        diagnostics = {
+                            enable = vim.g.use_bacon_lsp == false,
+                        }
+                    }
+                },
+            },
+        }
+
+        vim.lsp.config("rust_analyzer", {
+            server = {
+                cmd = function()
+                    local mason_registry = require('mason-registry')
+                    if mason_registry.is_installed('rust-analyzer') then
+                        -- This may need to be tweaked depending on the operating system.
+                        local ra = mason_registry.get_package('rust-analyzer')
+                        local ra_filename = ra:get_receipt():get().links.bin['rust-analyzer']
+                        return { ('%s/%s'):format(ra:get_install_path(), ra_filename or 'rust-analyzer') }
+                    else
+                        -- global installation
+                        return { 'rust-analyzer' }
+                    end
                 end,
             },
+        })
+
+        vim.lsp.config("tailwindcss", {
+            filetypes = { "html", "css", "scss", "javascript", "javascriptreact",
+                "typescript", "typescriptreact", "svelte", "rust" },
+            settings = {
+                tailwindCSS = {
+                    includeLanguages = { rust = "html" }
+                }
+            }
+        })
+
+        vim.lsp.config("emmet_ls", {
+            filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "rust" }
         })
 
         -- local lspconfig = require("lspconfig")

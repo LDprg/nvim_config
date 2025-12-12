ce29115e11c414d3bc9eb6523ba7156d26ec1b2d
LDprg <lukas_4dr@gmx.at>
2025-12-12 22:10:24 +0100

    Add tree-sitter-grammar for better grammar info

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,41 +11,61 @@ local group = vim.api.nvim_create_augroup("autosave", {})
 
 -- autosave for disabling automatic formatting
 vim.api.nvim_create_autocmd("User", {
-    pattern = "AutoSaveWritePre",
-    group = group,
-    callback = function(opts)
-        if opts.data.saved_buffer ~= nil then
-            local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
-            vim.g.autosave = filename
-        end
-    end,
+	pattern = "AutoSaveWritePre",
+	group = group,
+	callback = function(opts)
+		if opts.data.saved_buffer ~= nil then
+			local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+			vim.g.autosave = filename
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("User", {
-    pattern = "AutoSaveWritePost",
-    group = group,
-    callback = function(opts)
-        if opts.data.saved_buffer ~= nil then
-            vim.g.autosave = nil
-        end
-    end,
+	pattern = "AutoSaveWritePost",
+	group = group,
+	callback = function(opts)
+		if opts.data.saved_buffer ~= nil then
+			vim.g.autosave = nil
+		end
+	end,
 })
 
 -- autosave_enabled global for lualine
 vim.api.nvim_create_autocmd("User", {
-    pattern = "AutoSaveEnable",
-    group = group,
-    callback = function(_)
-        vim.g.autosave_enabled = true
-        -- vim.notify('AutoSave enabled', vim.log.levels.INFO)
-    end,
+	pattern = "AutoSaveEnable",
+	group = group,
+	callback = function(_)
+		vim.g.autosave_enabled = true
+		-- vim.notify('AutoSave enabled', vim.log.levels.INFO)
+	end,
 })
 
 vim.api.nvim_create_autocmd("User", {
-    pattern = "AutoSaveDisable",
-    group = group,
-    callback = function(_)
-        vim.g.autosave_enabled = false
-        -- vim.notify('AutoSave disabled', vim.log.levels.INFO)
-    end,
+	pattern = "AutoSaveDisable",
+	group = group,
+	callback = function(_)
+		vim.g.autosave_enabled = false
+		-- vim.notify('AutoSave disabled', vim.log.levels.INFO)
+	end,
 })
+
+if vim.fn.executable("/opt/tree-sitter-grammar-lsp/tree-sitter-grammar-lsp-linux") == 1 then
+	vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+		pattern = { "grammar.js", "*/corpus/*.txt" },
+		callback = function(ev)
+			vim.lsp.start({
+				name = "tree-sitter-grammar",
+				cmd = { "/opt/tree-sitter-grammar-lsp/tree-sitter-grammar-lsp-linux" },
+				root_dir = vim.fs.dirname(ev.file),
+				handlers = {
+					["window/showMessage"] = function(_, result)
+						if result.type == vim.lsp.log_levels.ERROR then
+							Snacks.notify.error(result.message)
+						end
+					end,
+				},
+			})
+		end,
+	})
+end

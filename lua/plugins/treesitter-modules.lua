c80731b125f780c547b4ffde6751d0221174bda2
LDprg <lukas_4dr@gmx.at>
2025-12-27 15:29:09 +0100

    move incremental selection to ts modules plugin

---
+++ lua/plugins/treesitter-modules.lua
@@ -0,0 +1,20 @@
+return {
+	"MeanderingProgrammer/treesitter-modules.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	---@module 'treesitter-modules'
+	---@type ts.mod.UserConfig
+	opts = {
+		incremental_selection = {
+			enable = true,
+			-- disable = false,
+			-- set value to `false` to disable individual mapping
+			-- node_decremental captures both node_incremental and scope_incremental
+			keymaps = {
+				init_selection = "<C-cr>",
+				node_incremental = "<C-cr>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+	},
+}
